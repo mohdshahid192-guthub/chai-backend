ade096d0302b5865a4ab89b4be73170b74e607a6
mohdshahid192-guthub <shahidroast192@gmail.com>
2026-03-30 23:39:06 +0530

    controllers and routes basic

--- src/app.js
+++ src/app.js
@@ -18,5 +18,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes
+import userRouter from "../src/routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+
 
 export { app }
